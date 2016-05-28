0ceefdf608ff469403e0b99fc097901a52ab2459
Michael Selik <mike@selik.org>
2016-05-28 12:35:18 -0400

    add visual signal of doctests running

--- tests/test_documentation.py
+++ tests/test_documentation.py
@@ -4,5 +4,16 @@ Verify all docstring examples are working.
 
 import doctest
 
-import destructure
-doctest.testmod(destructure)
+
+
+def test(module):
+    doctest.testmod(module)
+    print('.', end='')
+
+
+
+print('Doctests:', end=' ')
+
+import destructure; test(destructure)
+
+print()
